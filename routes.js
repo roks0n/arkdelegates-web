31a7f16051f7bdfd965de10121e2e7193ee5f579
roks0n <haluzan.rok@gmail.com>
2019-06-23 11:39:49 +0200

    add routes with slash

--- routes.js
+++ routes.js
@@ -2,11 +2,11 @@ const nextRoutes = require('next-routes')
 const routes = (module.exports = nextRoutes())
 
 routes
-  .add('delegate', '/delegate/:slug')
+  .add('delegate', '/delegate/:slug/')
   .add({
     name: 'delegate-contributions',
     page: 'delegate',
-    pattern: '/delegate/:slug/contributions',
+    pattern: '/delegate/:slug/contributions/',
   })
-  .add({ name: 'delegate-news', page: 'delegate', pattern: '/delegate/:slug/news' })
-  .add({ name: 'claim-delegate', page: 'auth/claim', pattern: '/auth/claim/:slug' })
+  .add({ name: 'delegate-news', page: 'delegate', pattern: '/delegate/:slug/news/' })
+  .add({ name: 'claim-delegate', page: 'auth/claim', pattern: '/auth/claim/:slug/' })
